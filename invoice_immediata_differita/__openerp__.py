b8ce960c3b3f794bb78768d8480df2a97df74dc0
Francesco Apruzzese <cescoap@gmail.com>
2012-09-17 20:16:35 +0200

    [IMP] Print management

--- invoice_immediata_differita/__openerp__.py
+++ invoice_immediata_differita/__openerp__.py
@@ -15,7 +15,11 @@
     'category': 'Finance',
     'description': """
     ITA : Modulo per la separazione tra fatture immediate e differite (Utile come base per moduli più complessi)
+          Impostare in ir_config_parameter due valori con chiavi 'report_invoice_immediate' e 'report_invoice_differate' e come
+          valori i nomi dei report da richiamare in fase di stampa
     ENG: Module for the separation of immediate and deferred invoices (Useful as a basis for more complex modules)
+          Set in ir_config_parameter two values ​​with keys 'report_invoice_immediate' and 'report_invoice_differate' and how
+          values ​​the names of reports to call for print
     """,
     'author': 'www.andreacometa.it',
     'website': 'http://www.andreacometa.it',
@@ -23,5 +27,5 @@
     "active": False,
     "installable": True,
     "depends" : ['account',],
-    "update_xml" : [],
+    "update_xml" : ['account/account_view.xml',],
 }
--- invoice_immediata_differita/account/account.py
+++ invoice_immediata_differita/account/account.py
@@ -17,6 +17,26 @@ class account_invoice(osv.osv):
 	_name = "account.invoice"
 	_inherit = "account.invoice"
 
+	def print_imm_diff_invoice(self, cr, uid, ids, context=None):
+		ir_values_obj = self.pool.get('ir.config_parameter')
+		if self.browse(cr, uid, ids, context)[0].immediate:
+			report_name = ir_values_obj.get_param(cr, uid, 'report_invoice_immediate', False)
+		else:
+			report_name = ir_values_obj.get_param(cr, uid, 'report_invoice_differate', False)
+		if not report_name:
+			raise osv.except_osv(_('Attenzione'), _('Impostare un report di stampa'))
+			return False
+		return {
+			'type':'ir.actions.report.xml',
+			'report_name': report_name,
+			'datas': {
+				'model':'account.invoice',
+				'ids': ids,
+				'report_type': 'pdf',
+			},
+			'nodestroy': True,
+		}
+
 	_columns = {
 		'immediate' : fields.boolean('Immediate'),
 	}
