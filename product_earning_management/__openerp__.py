0678019a8609819e9ea0cdb068272d3a114f0ea9
Francesco Apruzzese <cescoap@gmail.com>
2012-11-14 19:55:08 +0100

    [ADD] product_earning_management

---
+++ product_earning_management/__openerp__.py
@@ -0,0 +1,31 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Personalizzazione realizzata da Francesco OpenCode Apruzzese (f.apruzzese@andreacometa.it)
+#    Compatible with OpenERP release 6.1.X
+#    Copyright (C) 2010 Andrea Cometa. All Rights Reserved.
+#    Email: info@andreacometa.it, f.apruzzese@andreacometa.it
+#    Web site: http://www.andreacometa.it
+#
+##############################################################################
+
+{
+    'name': "product_earning_management",
+    'version': '0.1',
+    'category': 'Product',
+    'description': """
+    ITA: Permette di gestire il guadagno percentuale che si ha su un prodotto
+    ENG: It's possibile to manage the earning on the products prices""",
+    'author': 'www.andreacometa.it',
+    'website': 'http://www.andreacometa.it',
+    'license': 'AGPL-3',
+    "depends" : ['product'],
+    "init_xml" : [],
+    "update_xml" : [
+        'product/product_view.xml',
+        ],
+    "demo_xml" : [],
+    "active": False,
+    "installable": True,
+    "images" : [],
+}
---
+++ product_earning_management/product/product.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#    
+#    Copyright (C) 2012 Francesco OpenCode Apruzzese (<cescoap@gmail.com>)
+#    All Rights Reserved
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+
+from osv import fields, osv
+from tools.translate import _
+
+
+class product_product(osv.osv):
+
+	_name = "product.product"
+	_inherit = "product.product"
+
+	def name_search(self, cr, user, name, args=None, operator='ilike', context=None, limit=100):
+		if name:
+			args = ['|', ('alternative_code', operator, '%' + name + '%')] + args
+		res = super(product_product,self).name_search(cr, user, name, args, operator='ilike', context=None, limit=100)
+		return res
+
+	def _earning(self, cr, uid, ids, name, arg, context=None):
+		res = {}
+		prds = self.browse(cr, uid, ids)
+		for prd in prds:
+			earn_percentage = (prd.list_price / prd.standard_price) - 1
+			earn_fixed = prd.list_price - prd.standard_price
+			res[prd.id] = {'earning_percentage': earn_percentage, 'earning_fixed': earn_fixed}
+		return res
+
+	_columns = {
+		'earning_percentage' : fields.function(_earning, string="Percentage Earning", type='float', store=False, multi='earn'),
+		'earning_fixed' : fields.function(_earning, string="Fixed Earning", type='float', store=False, multi='earn'),
+		}
+
+product_product()
