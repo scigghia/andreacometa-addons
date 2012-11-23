2d3edcaa5eb703c7313c311bbb404626e96fc6dd
Andrea Cometa <info@andreacometa.it>
2012-11-23 11:58:54 +0100

    [imp] account_payment_term_enhanced: aggiunto giorno iniziale per il calcolo delle scadenze

--- account_payment_term_enhanced/account.py
+++ account_payment_term_enhanced/account.py
@@ -1,11 +1,23 @@
 # -*- encoding: utf-8 -*-
 ##############################################################################
 #
-#    Addon realizzato da Andrea Cometa
-#    Compatible with OpenERP release 6.1.0
-#    Copyright (C) 2012 Andrea Cometa. All Rights Reserved.
-#    Email: info@andreacometa.it
-#    Web site: http://www.andreacometa.it
+#    OpenERP, Open Source Management Solution
+#    Copyright (c) 2012 Andrea Cometa All Rights Reserved.
+#                       www.andreacometa.it
+#                       openerp@andreacometa.it
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 
@@ -27,12 +39,14 @@ class account_payment_term(osv.osv):
 			help="Day of the month, set -1 for the last day of the current month. If it's positive, it gives the day of the next month. Set 0 for net days (otherwise it's based on the beginning of the month)."),
 		'maturities' : fields.integer("Number of maturities", 
 			help="Number of maturities to divide the total amount"),
+		'start_day': fields.integer('Start Day', required=True, help="Start Day of the month, set -1 for the last day of the current month. If it's positive, it gives the day of the next month. Set 0 for net days (otherwise it's based on the beginning of the month)."),
 	}
 
 	_defaults = {
 		'maturities' : 1,
 		'days': 0,
 		'days2' : 0,
+		'start_day' : 0,
 		'fairly' : False,
 	}
 
@@ -47,18 +61,24 @@ class account_payment_term(osv.osv):
 		# added by me
 		if pt.fairly:
 			single_amount = round(amount / pt.maturities, prec)
-			next_date = datetime.strptime(date_ref, '%Y-%m-%d')
+			if pt.start_day < 0:
+				next_date = datetime.strptime(date_ref, '%Y-%m-%d') + relativedelta(day=1,months=1) + relativedelta(days=pt.start_day)
+			else:
+				next_date = datetime.strptime(date_ref, '%Y-%m-%d') + relativedelta(days=pt.start_day, months=1)
+			#next_date += relativedelta(days=pt.days) 
+
+			#####next_date = datetime.strptime(date_ref, '%Y-%m-%d')
 			#print "### %i %i %i" % (pt.maturities, pt.days, pt.days2)
 			for i in range(0, pt.maturities):
 				#print next_date, pt.days2
-				if i==0:
-					if pt.days2 < 0:
-						next_first_date = next_date + relativedelta(day=1,months=1) #Getting 1st of next month
-						next_date = next_first_date + relativedelta(days=pt.days2)
-					if pt.days2 > 0:
-						next_date += relativedelta(day=pt.days2, months=1)
-				else:
-					next_date += relativedelta(days=pt.days)
+				#if i==0:
+				if pt.days2 < 0:
+					next_first_date = next_date + relativedelta(day=1,months=1) #Getting 1st of next month
+					next_date = next_first_date + relativedelta(days=pt.days2)
+				if pt.days2 > 0:
+					next_date += relativedelta(day=pt.days2, months=1)
+				#else:
+				#	next_date += relativedelta(days=pt.days)
 				if i < pt.maturities-1:
 					#print "### STEP"
 					result.append( (next_date.strftime('%Y-%m-%d'), single_amount) )
@@ -76,7 +96,11 @@ class account_payment_term(osv.osv):
 				elif line.value == 'balance':
 					amt = round(amount, prec)
 				if amt:
-					next_date = (datetime.strptime(date_ref, '%Y-%m-%d') + relativedelta(days=line.days))
+					if line.start_day < 0:
+						start_date = datetime.strptime(date_ref, '%Y-%m-%d') + relativedelta(day=1,months=1) + relativedelta(days=line.start_day)
+					else:
+						start_date = datetime.strptime(date_ref, '%Y-%m-%d') + relativedelta(days=line.start_day, months=1)
+					next_date = (start_date + relativedelta(days=line.days) )
 					if line.days2 < 0:
 						next_first_date = next_date + relativedelta(day=1,months=1) #Getting 1st of next month
 						next_date = next_first_date + relativedelta(days=line.days2)
@@ -88,3 +112,13 @@ class account_payment_term(osv.osv):
 
 account_payment_term()
 
+class account_payment_term_line(osv.osv):
+	_inherit = 'account.payment.term.line'
+
+	_columns = {
+		'start_day': fields.integer('Start Day', required=True, help="Start Day of the month, set -1 for the last day of the current month. If it's positive, it gives the day of the next month. Set 0 for net days (otherwise it's based on the beginning of the month)."),
+	}
+	_defaults = {
+		'start_day' : 0,
+	}
+account_payment_term_line()
