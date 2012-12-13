fa8f9d69b735a42c99a25ae3803cc5df913c3795
Francesco Apruzzese <cescoap@gmail.com>
2012-12-13 16:34:21 +0100

    [ADD] Last Purchase Price Product module

---
+++ last_purchase_price_product/__openerp__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2011 www.andreacometa.it (<www.andreacometa.it>). All Rights Reserved
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    "name" : "last_purchase_price_product",
+    "description" : """
+        Adds LIFO as cost methos in products
+        Aggiunge l'ultimo prezzo di acquisto automatico
+        """,
+    "version" : "1.0",
+    "author" : "www.andreacometa.it",
+    "depends" : ["product","stock"],
+    "category" : "Product",
+    "init_xml" : [],
+    "update_xml" : [
+        'product/product_view.xml'
+    ],
+    'demo_xml': [],
+    "website": 'http://www.andreacometa.it',
+    'installable': True,
+    'active': False,
+}
---
+++ last_purchase_price_product/wizard/wizard_partial_picking.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2011 www.andreacometa.it (<www.andreacometa.it>). All Rights Reserved
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from osv import osv
+
+class partial_picking(osv.osv_memory):
+
+	_inherit = 'stock.partial.picking'
+
+	def do_partial(self, cr, uid, ids, context=None):
+		if context is None:
+			context = {} 
+		pick_obj = self.pool.get('stock.picking')
+		purchase_obj = self.pool.get('purchase.order')
+		picking_ids = context.get('active_ids', False)
+		partial = self.browse(cr, uid, ids[0], context=context)
+		pick_list = pick_obj.browse(cr, uid, picking_ids, context=context)
+		for pick in pick_list:
+			if pick.type == 'in' and pick.origin:
+				order_id = purchase_obj.search(cr, uid, [('name','=',pick.origin)], limit=1)[0]
+				if order_id:
+					order = purchase_obj.browse(cr, uid, order_id)
+					for move in partial.move_ids:
+						if move.product_id.cost_method == 'lpp':
+							for line in order.order_line:
+								product_cost = 0.0
+								if line.product_id.id == move.product_id.id:
+									product_cost = line.price_unit
+							self.pool.get('product.product').write(cr, uid, [move.product_id.id], {'standard_price': product_cost})
+		return super(partial_picking, self).do_partial(cr, uid, ids, context=context)
+
+partial_picking()
