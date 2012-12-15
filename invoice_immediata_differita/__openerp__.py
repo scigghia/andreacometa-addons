bce9f972d119b7e6da0f5a182fe3f91d970e87d9
Andrea Cometa <info@andreacometa.it>
2012-12-15 16:33:59 +0100

    [imp] invoice_immediata_differita: aggiunto flag in view (fattura immediata/differita) per gli account_manager

--- invoice_immediata_differita/__openerp__.py
+++ invoice_immediata_differita/__openerp__.py
@@ -1,13 +1,25 @@
 # -*- encoding: utf-8 -*-
 ##############################################################################
 #
-#    Personalizzazione realizzata da Francesco OpenCode Apruzzese (f.apruzzese@andreacometa.it)
-#    Compatible with OpenERP release 6.1.X
-#    Copyright (C) 2012 Andrea Cometa. All Rights Reserved.
-#    Email: cescoap@gmail.com, info@andreacometa.it
-#    Web site: http://www.andreacometa.it
+#    OpenERP, Open Source Management Solution
+#    Copyright (c) 2012 Andrea Cometa All Rights Reserved.
+#                       www.andreacometa.it
+#                       openerp@andreacometa.it
 #
-##########################################################################
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
 
 {
     'name': 'Invoice Immediata-Differita',
@@ -15,10 +27,10 @@
     'category': 'Finance',
     'description': """
     ITA : Modulo per la separazione tra fatture immediate e differite (Utile come base per moduli più complessi)
-          Impostare in ir_config_parameter due valori con chiavi 'report_invoice_immediate' e 'report_invoice_differate' e come
+          Impostare in ir_config_parameter due valori con chiavi 'report_invoice_immediate' e 'report_invoice_differite' e come
           valori i nomi dei report da richiamare in fase di stampa
     ENG: Module for the separation of immediate and deferred invoices (Useful as a basis for more complex modules)
-          Set in ir_config_parameter two values ​​with keys 'report_invoice_immediate' and 'report_invoice_differate' and how
+          Set in ir_config_parameter two values ​​with keys 'report_invoice_immediate' and 'report_invoice_diffirate' and how
           values ​​the names of reports to call for print
     """,
     'author': 'www.andreacometa.it',
