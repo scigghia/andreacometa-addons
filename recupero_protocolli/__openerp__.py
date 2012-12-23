d7a0a558fc8ad2c2372a12d1afbf9acb064a6ca5
Andrea Cometa <info@andreacometa.it>
2012-12-23 10:26:22 +0100

    intestazioni

--- recupero_protocolli/__openerp__.py
+++ recupero_protocolli/__openerp__.py
@@ -1,8 +1,10 @@
 # -*- encoding: utf-8 -*-
 ##############################################################################
-#    
-#    Copyright (C) 2011 Francesco OpenCode Apruzzese (<http://www.andreacometa.it>). 
-#    All Rights Reserved
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (c) 2012 Andrea Cometa All Rights Reserved.
+#                       www.andreacometa.it
+#                       openerp@andreacometa.it
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published by
@@ -12,22 +14,23 @@
 #    This program is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
+#    GNU Affero General Public License for more details.
 #
-#    You should have received a copy of the GNU General Public License
+#    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
+
 {
 	'name': 'Modulo per la gestione del recupero dei buchi dei protocolli fiscali',
-	'version': '0.1',
+	'version': '1.0',
 	'category': 'Custom',
 	'description': 
 		"""
 		Modulo per la gestione del recupero dei buchi delle sequence (DDT, Fatture, etc.)
 		Basta ereditare l'unlink di una classe con sequence per ottenere la funzionalità di ripristino
 		""",
-	'author': 'Apruzzese Francesco',
+	'author': 'www.andreacometa.it',
 	'website': 'http://www.andreacometa.it',
 	'license': 'AGPL-3',
 	"active": False,
