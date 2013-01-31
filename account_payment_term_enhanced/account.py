abff56ca743c6f8513339d292ac494cd2a9cb2f1
Andrea Cometa <info@andreacometa.it>
2013-01-31 21:13:55 +0100

    [imp] calc by month, [add] language pot

--- account_payment_term_enhanced/account.py
+++ account_payment_term_enhanced/account.py
@@ -40,14 +40,17 @@ class account_payment_term(osv.osv):
 		'maturities' : fields.integer("Number of maturities", 
 			help="Number of maturities to divide the total amount"),
 		'start_day': fields.integer('Start Day', required=True, help="Start Day of the month, set -1 for the last day of the current month. If it's positive, it gives the day of the next month. Set 0 for net days (otherwise it's based on the beginning of the month)."),
+		'monthly' : fields.boolean("Calculate by month",
+			help="if checked, calculates one maturity for every month"),
 	}
 
 	_defaults = {
-		'maturities' : 1,
-		'days': 0,
-		'days2' : 0,
-		'start_day' : 0,
-		'fairly' : False,
+		'maturities':	1,
+		'days':			0,
+		'days2':		0,
+		'start_day':	0,
+		'fairly' :		False,
+		'monthly':		False,
 	}
 
 	def compute(self, cr, uid, id, value, date_ref=False, context=None):
@@ -65,17 +68,23 @@ class account_payment_term(osv.osv):
 				start_date = datetime.strptime(date_ref, '%Y-%m-%d') + relativedelta(day=1,months=1) + relativedelta(days=pt.start_day)
 			else:
 				start_date = datetime.strptime(date_ref, '%Y-%m-%d') + relativedelta(days=pt.start_day, months=0)
-			print "data iniziale: %s\n" % start_date
+			#print "data iniziale: %s\n" % start_date
 
 			for i in range(0, pt.maturities):
 				if pt.maturities>1:
-					next_date = (start_date + relativedelta(days=pt.days * (i+1)))
+					if pt.monthly:
+						next_date = (start_date + relativedelta(months= (i+1)))
+					else:
+						next_date = (start_date + relativedelta(days=pt.days * (i+1)))
 					if pt.days2 < 0:
 						next_first_date = next_date + relativedelta(day=1,months=1) #Getting 1st of next month
 						next_date = next_first_date + relativedelta(days=pt.days2)
 					if pt.days2 > 0: # numero giorno in cui farlo scadere
-						next_date += relativedelta(day=pt.days2, months=1)
-					print "data scadenza: %s\n" % next_date
+						if pt.monthly:
+							next_date += relativedelta(day=pt.days2)
+						else:
+							next_date += relativedelta(day=pt.days2, months=1)
+					#print "data scadenza: %s\n" % next_date
 				else:
 					next_date = start_date
 
